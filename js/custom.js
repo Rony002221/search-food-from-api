9ff405fda61b4f134f8bedb6be77d010f1919173
rony <rony49883@gmail.com>
2021-09-01 19:54:51 +0600

    fetch data from mealDB api

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,13 @@
-console.log("connected")
+const search_food = () => {
+    const search_box = document.getElementById('search_box');
+    const search_value = search_box.value;
+    search_box.value = "";
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search_value}`;
+
+    //console.log(url)
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
